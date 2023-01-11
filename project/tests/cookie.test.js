b4b4bddc843c75712700c3d2c83f1de33a9a0d3f
liolle <laurentlionel20@hotmail.com>
2023-01-11 16:46:05 +0100

    Tests

---
+++ project/tests/cookie.test.js
@@ -0,0 +1,21 @@
+
+
+test('test1', () => {
+    //TODO
+});
+
+test('test2', () => {
+    //TODO
+});
+
+test('test3', () => {
+    //TODO
+});
+
+test('test4', () => {
+    //TODO
+});
+
+test('test5', () => {
+    //TODO
+});
