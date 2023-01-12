6b1a1b28d2f10a052beed7204ac11fd472399161
yvsmlk <yves-mp@hotmail.be>
2023-01-12 14:05:08 +0100

    niew

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var clicks = 0;
+
+document.querySelector('.img').addEventListener('click',()=>{
+    function increment() {clicks = clicks + 1;
+        counter.innerHTML = clicks;
+        
+}
+increment()
+document.querySelector('score').textContent
+        
+});
