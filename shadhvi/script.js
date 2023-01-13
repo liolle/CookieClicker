ec24c5a3b1d1a43a3977a4a6197c8957dcda3bf2
ShadhviVR <v.r.shadhvi@gmail.com>
2023-01-13 12:23:38 +0100

    cookie

--- shadhvi/script.js
+++ shadhvi/script.js
@@ -1,8 +1,5 @@
 var num = 0;
-
-
 var cookie = document.getElementById("cookie");
-
 function cookieClick() { 
     num += 1;
 
@@ -10,3 +7,19 @@ function cookieClick() {
     numbers.innerHTML = num;      
 }
 
+document.addEventListener("click", (e) => {
+var cookieFall = document.createElement("span");
+cookieFall.classList.add("cookieFall");
+
+cookieFall.style.left = e.offsetX + "px";
+cookieFall.style.top = e.offsetY + "px";
+
+var size = Math.random() * (100 -20 +1) +20;
+
+cookieFall.style.width = size + "px";
+cookieFall.style.height = size + "px";
+document.body.appendChild(cookieFall);
+setTimeout(() => {
+cookieFall.remove();
+}, 1000);
+});
