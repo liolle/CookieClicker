c57b69f18d1cb5564813e7952fa444904a6eace2
yvsmlk <yves-mp@hotmail.be>
2023-01-16 13:36:53 +0100

    changement du multiplicateur

--- src/main.js
+++ src/main.js
@@ -10,16 +10,22 @@ const bonusTripleImg = document.getElementById('bonus-triple-img');
 const bonusAutoImg = document.getElementById('bonus-auto-img');
 const bonusBoost = document.getElementById('bonus-boost');
 const bonusBoostImg = document.getElementById('bonus-boost-img');
+const spentMoney= document.getElementById('spent');
+const buttonRest = document.getElementById('rest');
 let boost=1;
+const click=0;
 const titre=document.getElementById('titre');
 
  // ===== Functions =====
+ 
+
  function readMultiplicator(){
   return Number(multiplicator.value);       // Lire la valeur dans l'input multiplicateur
  }
 
-function buy(price, bonus, image,message){
-    counter.stepDown(price);                // Retirer 10 au score
+function buy(price, bonus, image,message,spent){
+    counter.stepDown(price);
+    spentMoney.stepUp(price);                // Retirer 10 au score
     bonus.disabled=true;                    // Désactiver le boutton
     titre.textContent=message;
     image.style.filter='grayscale(1)';      // Image grisée
@@ -32,31 +38,40 @@ cookie.addEventListener('click', () => {
 
 bonusDouble.addEventListener('click', () => {
     if(counter.value>=10){                  // Si le score est supérieur à 10
-        buy(10, bonusDouble, bonusDoubleImg,'bonus double');
+        buy(10, bonusDouble, bonusDoubleImg,'Well done you doubled your score',10);
         multiplicator.stepUp(1);            // Augmenter le multiplicateur
     }
 });
 
 bonusTriple.addEventListener('click', () => {
     if(counter.value>=30){
-        buy(30, bonusTriple, bonusTripleImg,'bonus triple');
+        buy(30, bonusTriple, bonusTripleImg,'great you have now tripled the score',30);
         multiplicator.stepUp(3)
     }
 });
 
-bonusAutoclick.addEventListener('click', () => {
-    if(counter.value>=50){
-        buy(50, bonusAutoclick, bonusAutoImg,'super boost');
-        setInterval(() => cookie.click(), 1000); // Click toute les 1 secondes
-    }  
-});
+
 
 bonusBoost.addEventListener('click',()=>{
-        if(counter.value>=25){
-            buy(25,bonusBoost,bonusBoostImg,'boost 2');
+        if(counter.value>=140){
+            buy(140,bonusBoost,bonusBoostImg,'beautiful you have 30 seconds click a maximum to increase your cookies',140);
             boost=2;
             setTimeout(()=>boost=1, 30000);
 
 
         }
+       
+});
+bonusAutoclick.addEventListener('click', () => {
+    if(counter.value>=670){
+        buy(670, bonusAutoclick, bonusAutoImg,'you are a champion you can rest I continue',670);
+        setInterval(() => cookie.click(), 1000); // Click toute les 1 secondes
+    }  
+});
+
+buttonRest.addEventListener('click',()=>{
+   counter.value=0;
+   spentMoney.value=0;
+   multiplicator.value=1;
+    
 });
