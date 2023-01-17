d0e7e39d4e1abd59d8ab1479365c43eca4c0345f
liolle <laurentlionel20@hotmail.com>
2023-01-17 17:07:21 +0100

    c2

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import {cookieClick, randomRange, getFraction, autoClick,getScore, substractScore, addScrore, buy_bonus } from './lib';
+
 // ===== HTML =====
 const cookie = document.getElementById('cookie');
 const counter = document.getElementById('counter');
@@ -18,44 +20,109 @@ const titre = document.getElementById('titre');
 
  // ===== Functions =====
  
+let bnt_states = [false,false,false,false,false,false]
 
- function readMultiplicator(){
-  return Number(multiplicator.value);       // Lire la valeur dans l'input multiplicateur
- }
-
-function buy(price, bonus, image,message,spent){
-    counter.stepDown(price);
-    spentMoney.stepUp(price);                // Retirer 10 au score
-    bonus.disabled=false;                    // Désactiver le boutton
-    titre.textContent=message;
-    image.style.filter='hue-rotate(90deg)';      // Image grisée
+const bonus_map = {
+    autoclicker: {price:10,duration:15,frequency:5,price_multiplier:1.5},
 }
 
-function buy1(price, bonus, image,message,spent){
-    counter.stepDown(price);
-    spentMoney.stepUp(price);                // Retirer 10 au score
-    bonus.disabled=false;                    // Désactiver le boutton
-    titre.textContent=message;
-    image.style.filter='contrast(200%)';      // Image grisée
-}
+function readMultiplicator(){
+    return Number(multiplicator.value);       // Lire la valeur dans l'input multiplicateur
+   }
+  
+  function buy(price, bonus, image,message,spent){
+      counter.stepDown(price);
+      spentMoney.stepUp(price);                // Retirer 10 au score
+      bonus.disabled=false;                    // Désactiver le boutton
+      titre.textContent=message;
+      image.style.filter='hue-rotate(90deg)';      // Image grisée
+  }
+  
+  function buy1(price, bonus, image,message,spent){
+      counter.stepDown(price);
+      spentMoney.stepUp(price);                // Retirer 10 au score
+      bonus.disabled=false;                    // Désactiver le boutton
+      titre.textContent=message;
+      image.style.filter='contrast(200%)';      // Image grisée
+  }
+  
+  function buy2(price, bonus, image,message,spent){
+      counter.stepDown(price);
+      spentMoney.stepUp(price);                // Retirer 10 au score
+      bonus.disabled=false;                    // Désactiver le boutton
+      titre.textContent=message;
+      image.style.filter='opacity(25%)';      // Image grisée
+  }
+  
+  function buy3(price, bonus, image,message,spent){
+      counter.stepDown(price);
+      spentMoney.stepUp(price);                // Retirer 10 au score
+      bonus.disabled=true;                    // Désactiver le boutton
+      titre.textContent=message;
+      image.style.filter='grayscale(1)';      // Image grisée
+  }
 
-function buy2(price, bonus, image,message,spent){
-    counter.stepDown(price);
-    spentMoney.stepUp(price);                // Retirer 10 au score
-    bonus.disabled=false;                    // Désactiver le boutton
-    titre.textContent=message;
-    image.style.filter='opacity(25%)';      // Image grisée
-}
 
-function buy3(price, bonus, image,message,spent){
-    counter.stepDown(price);
-    spentMoney.stepUp(price);                // Retirer 10 au score
-    bonus.disabled=true;                    // Désactiver le boutton
-    titre.textContent=message;
-    image.style.filter='grayscale(1)';      // Image grisée
-}
 
-// ===== Event Listeners =====
+/*
+
+--------------------------------------------------------------------------------
+                                Listeners
+--------------------------------------------------------------------------------
+*/
+
+const score_container = document.querySelector("#numbers")
+const score_container2 = document.getElementById("numbers")
+
+
+document.querySelector('#btn1').addEventListener('click', ()=>{
+    
+    if(bnt_states[0] || !buy_bonus('autoclicker',bonus_map)) return
+
+    bnt_states[0] = true
+    
+    autoClick(bonus_map['autoclicker']['duration'],bonus_map['autoclicker']['frequency'])
+    .then(()=>{
+        bnt_states[0] = false
+    })
+    
+    
+})
+
+document.getElementById("cookie").addEventListener('click',()=>{
+    cookieClick()
+})
+
+document.getElementById("cookie").addEventListener("click", (e) => {
+    var cookieFall = document.createElement("span");
+    cookieFall.classList.add("cookieFall");
+
+    //Adding a bite mor randomness 
+    let w = window.innerWidth 
+    let h = window.innerHeight 
+
+    let offsetX = getFraction(w)
+    let offsetY = getFraction(h,5)
+    
+    // cookieFall.style.left = e.offsetX + "px";
+    // cookieFall.style.top = e.offsetY + "px";
+    let size = Math.random() * (100 -20 +1) +20;
+
+    cookieFall.style.left = randomRange(offsetX,w-offsetX-size) + "px";
+    cookieFall.style.top = randomRange(offsetY,Math.ceil(h/4)) + "px";
+
+    
+    
+    cookieFall.style.width = size + "px";
+    cookieFall.style.height = size + "px";
+    document.body.appendChild(cookieFall);
+    setTimeout(() => {
+    cookieFall.remove();
+    }, 2000);
+});
+
+
+
 cookie.addEventListener('click', () => {
     counter.stepUp(readMultiplicator() * boost);    // Incrémenter le score de la valeur du multiplicateur
 });
@@ -66,7 +133,7 @@ bonusDouble.addEventListener('click', () => {
         multiplicator.stepUp(1);            // Augmenter le multiplicateur
     }
 });
-   
+    
 
 bonusTriple.addEventListener('click', () => {
     if(counter.value>=30){
@@ -85,7 +152,7 @@ bonusBoost.addEventListener('click',()=>{
 
 
         }
-       
+        
 });
 bonusAutoclick.addEventListener('click', () => {
     if(counter.value>=670){
@@ -95,38 +162,9 @@ bonusAutoclick.addEventListener('click', () => {
 });
 
 buttonRest.addEventListener('click',()=>{
-   counter.value=0;
-   spentMoney.value=0;
+    counter.value=0;
+    spentMoney.value=0;
     multiplicator.value=1;
 
     
 })
-
-document.getElementById("cookie").addEventListener("click", (e) => {
-    var cookieFall = document.createElement("span");
-    cookieFall.classList.add("cookieFall");
-
-    //Adding a bite mor randomness 
-    let w = window.innerWidth 
-    let h = window.innerHeight 
-
-    let offsetX = getFraction(w)
-    let offsetY = getFraction(h,5)
-    // cookieFall.style.left = e.offsetX + "px";
-    // cookieFall.style.top = e.offsetY + "px";
-    let size = Math.random() * (100 -20 +1) +20;
-
-    cookieFall.style.left = randomRange(offsetX,w-offsetX-size) + "px";
-    cookieFall.style.top = randomRange(offsetY,Math.ceil(h/4)) + "px";
-
-    
-    
-    cookieFall.style.width = size + "px";
-    cookieFall.style.height = size + "px";
-    document.body.appendChild(cookieFall);
-    setTimeout(() => {
-    cookieFall.remove();
-    }, 2000);
-});
-
-//------------------------
